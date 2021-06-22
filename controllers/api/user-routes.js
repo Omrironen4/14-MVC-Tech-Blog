bc9d678482b3b4bbdbb175cd54a822f65a504c16
Omri <omri.ronen4@gmail.com>
2021-06-21 18:18:10 -0700

    fixed user route with the login where it finds username instead of email

--- controllers/api/user-routes.js
+++ controllers/api/user-routes.js
@@ -69,7 +69,7 @@ try {
 // login
 router.post('/login', async(req, res) => {
  try {
-   const userData = await User.findOne({ where: { email: req.body.email } });
+   const userData = await User.findOne({ where: { username: req.body.username } });
 
    if (!userData) {
      res
