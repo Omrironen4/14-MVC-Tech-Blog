d2d4be7738b3167e0ac703f2748de1cacc7d9b82
Omri <omri.ronen4@gmail.com>
2021-06-24 00:07:09 -0700

    ok

--- seeds/comment-seeds.js
+++ seeds/comment-seeds.js
@@ -2,33 +2,33 @@ const {Comment} = require('../models');
 
 const commentData = [
     {
-        user_id: '2',
         comment: 'lol you fooled me!',
+        user_id: '2',
         post_id: '1', 
     },
     {
-        user_id: '1',
         comment: 'wow just saw it!',
+        user_id: '1',
         post_id: '2', 
     },
     {
-        user_id: '4',
         comment: 'Should average 20 minutes, very fast!',
+        user_id: '4',
         post_id: '3', 
     },
     {
-        user_id: '3',
         comment: 'I know a great one, call me (xxx)-(xxx)-(xxxx)!',
+        user_id: '3',
         post_id: '4', 
     },
     {
-        user_id: '6',
         comment: 'I have the same one!',
+        user_id: '6',
         post_id: '5', 
     },
     {
-        user_id: '5',
         comment: 'We can study together on the weekends!',
+        user_id: '5',
         post_id: '6', 
     },
 
--- seeds/post-seeds.js
+++ seeds/post-seeds.js
@@ -3,32 +3,32 @@ const {Post} = require('../models');
 const postData = [
     {
         title: 'Checkout the new mysql3!',
-        description: 'Just kidding it did not come out yet.',
+        content: 'Just kidding it did not come out yet.',
         user_id: 1,
     },
     {
         title: 'New VSC',
-        description: 'have you checkout the new cool color schema? Plus they have a bunch of new extensions available.',
+        content: 'have you checkout the new cool color schema? Plus they have a bunch of new extensions available.',
         user_id: 2,
     },
     {
         title: 'Mysql2 update',
-        description: 'How long does mysql update take?',
+        content: 'How long does mysql update take?',
         user_id: 3,
     },
     {
         title: 'Need a tutor for javascript',
-        description: 'Who knows a great tutor?',
+        content: 'Who knows a great tutor?',
         user_id: 4,
     },
     {
         title: 'New monitor is next level',
-        description: 'I would like to introduce my new monitor to you it is really cool!',
+        content: 'I would like to introduce my new monitor to you it is really cool!',
         user_id: 5,
     },
     {
         title: 'Finding a friend for my studies in CS',
-        description: 'I would like to find a friend to study CS with on the weekends!', 
+        content: 'I would like to find a friend to study CS with on the weekends!', 
         user_id: 6,
     },
 ];
